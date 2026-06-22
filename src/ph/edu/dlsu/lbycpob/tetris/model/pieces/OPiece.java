ff90b0676e6448c6c6ffe76df464da7827597ce0
cassandramazo-sys <cassandra_mazo@dlsu.edu.ph>
2026-06-22 19:41:53 +0800

    added missing code for OPiece

---
+++ src/ph/edu/dlsu/lbycpob/tetris/model/pieces/OPiece.java
@@ -0,0 +1,12 @@
+package ph.edu.dlsu.lbycpob.tetris.model.pieces;
+
+public class OPiece extends TetrominoBase {
+    public OPiece() {
+        super(new int[][][] {
+                {
+                        {1, 1},
+                        {1, 1}
+                }
+        }, TetrominoColor.YELLOW);
+    }
+}
